8a95ac562beba76dd560851d18d98e8000bfd8e5
AndresSalcedo28 <andressalcedo.28@outlook.com>
2020-05-27 00:22:45 -0500

    main, definition, presentation

--- lib/tabs/inicio_tabs.dart
+++ lib/tabs/inicio_tabs.dart
@@ -12,7 +12,7 @@ class InicioTabs extends StatelessWidget {
 
    _renderBg() {
     return Container(
-      decoration: BoxDecoration(color: const Color(0xFFFFFFFF)),
+      decoration: BoxDecoration(color: const Color(0xFFD7CCC8)),
     );
   }
 
@@ -41,29 +41,30 @@ class InicioTabs extends StatelessWidget {
         },
         front: Container(
           decoration: BoxDecoration(
-            color: Color(0xFF006666),
+            color: Color(0xFFDCEDC8),
             borderRadius: BorderRadius.all(Radius.circular(8.0)),
           ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('Front', style: Theme.of(context).textTheme.headline),
-              Text('Click here to flip back',
-                  style: Theme.of(context).textTheme.body1),
+              Text('SoClose', style: Theme.of(context).textTheme.headline),
+              Text('¡La mejor forma de encontrar a tus amigos!',
+                  style: Theme.of(context).textTheme.body1,),
+                  
             ],
           ),
         ),
         back: Container(
           decoration: BoxDecoration(
-            color: Color(0xFF006666),
+            color: Color(0xFFDCEDC8),
             borderRadius: BorderRadius.all(Radius.circular(8.0)),
           ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('Back', style: Theme.of(context).textTheme.headline),
-              Text('Click here to flip front',
-                  style: Theme.of(context).textTheme.body1),
+              Text('¡BIENVENDIDO!', style: Theme.of(context).textTheme.headline, textAlign: TextAlign.center),
+              Text('SoClose es una aplicación que te ayudara a encontrar a tus amigos en una ubicación determinada, más rápida y facilmente',
+                  style: Theme.of(context).textTheme.body1, textAlign: TextAlign.center,),
             ],
           ),
         ),
@@ -107,7 +108,7 @@ class InicioTabs extends StatelessWidget {
         onClose: () => print('DIAL CLOSED'),
         tooltip: 'Speed Dial',
         heroTag: 'speed-dial-hero-tag',
-        backgroundColor: Colors.white,
+        backgroundColor: Color(0xFFDCEDC8),
         foregroundColor: Colors.black,
         elevation: 8.0,
         shape: CircleBorder(),
