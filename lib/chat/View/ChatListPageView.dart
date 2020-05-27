bcb5faedd61ef42ed755d2b89cb807fafe711b15
AndresSalcedo28 <andressalcedo.28@outlook.com>
2020-05-27 17:21:24 -0500

    chatbot 2.0
    chatbot implemented with preset phrases

--- lib/chat/View/ChatListPageView.dart
+++ lib/chat/View/ChatListPageView.dart
@@ -1,125 +1,136 @@
+import 'package:bubble/bubble.dart';
 import 'package:flutter/material.dart';
-import 'package:login/chat/Global/Colors.dart' as myColors;
-import 'package:login/chat/Widget/ChatListViewItem.dart';
-import 'package:login/chat/Widget/Loading.dart';
+import 'package:flutter_dialogflow/dialogflow_v2.dart';
+
+
 
 class ChatListPageView extends StatefulWidget {
   @override
-  _ChatListPageViewState createState() => _ChatListPageViewState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _ChatListPageViewState extends State<ChatListPageView> {
-  bool isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-
-    Future.delayed(const Duration(seconds: 2), () {
-      setState(() {
-        isLoading = false;
+class _MyAppState extends State<ChatListPageView> {
+  void response(query) async {
+    AuthGoogle authGoogle = await AuthGoogle(
+            fileJson: "assets/soclose-eddfe-4f18ed7a3f54.json")
+        .build();
+    Dialogflow dialogflow =
+        Dialogflow(authGoogle: authGoogle, language: Language.english);
+    AIResponse aiResponse = await dialogflow.detectIntent(query);
+    setState(() {
+      messsages.insert(0, {
+        "data": 0,
+        "message": aiResponse.getListMessage()[0]["text"]["text"][0].toString()
       });
     });
   }
 
+  final messageInsert = TextEditingController();
+  List<Map> messsages = List();
+
   @override
   Widget build(BuildContext context) {
-    if (isLoading == true) {
-      return Loading();
-    } else {
-      return Container(
-        child: Scaffold(
-          backgroundColor: myColors.blue,
-          appBar: AppBar(
-            elevation: 0,
-            iconTheme: IconThemeData(
-              color: Colors.white,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "SoClose chat",
+        ),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+                child: ListView.builder(
+                    reverse: true,
+                    itemCount: messsages.length,
+                    itemBuilder: (context, index) => chat(
+                        messsages[index]["message"].toString(),
+                        messsages[index]["data"]))),
+            Divider(
+              height: 5.0,
+              color: Colors.deepOrange,
             ),
-            centerTitle: true,
-            title: Text(
-              'chats',
-              style: TextStyle(color: Colors.white, fontSize: 18),
-            ),
-          ),
-          body: Container(
-            child: Container(
-              decoration: BoxDecoration(
-                  color: myColors.backGround,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(15.0),
-                    topRight: Radius.circular(15.0),
-                  )),
-              child: ListView(
+            Container(
+              padding: EdgeInsets.only(left: 15.0, right: 15.0),
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
                 children: <Widget>[
-                  ChatListViewItem(
-                    hasUnreadMessage: true,
-                    image: AssetImage('assets/images/person1.jpg'),
-                    lastMessage:
-                        "Lorem ipsum dolor sit amet. Sed pharetra ante a blandit ultrices.",
-                    name: "Bree Jarvis",
-                    newMesssageCount: 8,
-                    time: "19:27 PM",
-                  ),
-                  ChatListViewItem(
-                    hasUnreadMessage: true,
-                    image: AssetImage('assets/images/person2.png'),
-                    lastMessage:
-                        "Lorem ipsum dolor sit amet. Sed pharetra ante a blandit ultrices.",
-                    name: "Alex",
-                    newMesssageCount: 5,
-                    time: "19:27 PM",
-                  ),
-                  ChatListViewItem(
-                    hasUnreadMessage: false,
-                    image: AssetImage('assets/images/person3.jpg'),
-                    lastMessage:
-                        "Lorem ipsum dolor sit amet. Sed pharetra ante a blandit ultrices.",
-                    name: "Carson Sinclair",
-                    newMesssageCount: 0,
-                    time: "19:27 PM",
-                  ),
-                  ChatListViewItem(
-                    hasUnreadMessage: false,
-                    image: AssetImage('assets/images/person4.png'),
-                    lastMessage:
-                        "Lorem ipsum dolor sit amet. Sed pharetra ante a blandit ultrices.",
-                    name: "Lucian Guerra",
-                    newMesssageCount: 0,
-                    time: "19:27 PM",
-                  ),
-                  ChatListViewItem(
-                    hasUnreadMessage: false,
-                    image: AssetImage('assets/images/person5.jpg'),
-                    lastMessage:
-                        "Lorem ipsum dolor sit amet. Sed pharetra ante a blandit ultrices.",
-                    name: "Sophia-Rose Bush",
-                    newMesssageCount: 0,
-                    time: "19:27 PM",
-                  ),
-                  ChatListViewItem(
-                    hasUnreadMessage: false,
-                    image: AssetImage('assets/images/person6.jpg'),
-                    lastMessage:
-                        "Lorem ipsum dolor sit amet. Sed pharetra ante a blandit ultrices.",
-                    name: "Mohammad",
-                    newMesssageCount: 0,
-                    time: "19:27 PM",
-                  ),
-                  ChatListViewItem(
-                    hasUnreadMessage: false,
-                    image: AssetImage('assets/images/person7.jpg'),
-                    lastMessage:
-                        "Lorem ipsum dolor sit amet. Sed pharetra ante a blandit ultrices.",
-                    name: "Jimi Cooke",
-                    newMesssageCount: 0,
-                    time: "19:27 PM",
-                  ),
+                  Flexible(
+                      child: TextField(
+                    controller: messageInsert,
+                    decoration: InputDecoration.collapsed(
+                        hintText: "Send your message",
+                        hintStyle: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18.0)),
+                  )),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 4.0),
+                    child: IconButton(
+                      
+                        icon: Icon(
+                          
+                          Icons.send,
+                          size: 30.0,
+                          color: Colors.deepOrange,
+                        ),
+                        onPressed: () {
+                          if (messageInsert.text.isEmpty) {
+                            print("empty message");
+                          } else {
+                            setState(() {
+                              messsages.insert(0,
+                                  {"data": 1, "message": messageInsert.text});
+                            });
+                            response(messageInsert.text);
+                            messageInsert.clear();
+                          }
+                        }),
+                  )
                 ],
               ),
             ),
-          ),
+            SizedBox(
+              height: 15.0,
+            )
+          ],
         ),
-      );
-    }
+      ),
+    );
+  }
+
+  //for better one i have use the bubble package check out the pubspec.yaml
+
+  Widget chat(String message, int data) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Bubble(
+          radius: Radius.circular(15.0),
+          color: data == 0 ? Colors.deepOrange : Colors.orangeAccent,
+          elevation: 0.0,
+          alignment: data == 0 ? Alignment.topLeft : Alignment.topRight,
+          nip: data == 0 ? BubbleNip.leftBottom : BubbleNip.rightTop,
+          child: Padding(
+            padding: EdgeInsets.all(2.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage(
+                      data == 0 ? "assets/images/person1.jpg" : "assets/images/person2.png"),
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Flexible(
+                    child: Text(
+                  message,
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ))
+              ],
+            ),
+          )),
+    );
   }
 }
